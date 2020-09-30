5b8f8e59399d381f7a7304bc978d07f94bdcac9d
mazhengyao <ma_zhengyao@163.com>
2020-09-30 17:19:04 +0800

    text not select

--- src/App.vue
+++ src/App.vue
@@ -246,6 +246,20 @@
     text-align: center;
     color: #2c3e50;
     /*margin-top: 60px;*/
+    -webkit-touch-callout:none;  /*系统默认菜单被禁用*/
+    -webkit-user-select:none; /*webkit浏览器*/
+    /*-khtml-user-select:none; !*早期浏览器*!*/
+    -moz-user-select:none;/*火狐*/
+    -ms-user-select:none; /*IE10*/
+    user-select:none;
+  }
+
+  input{
+    -webkit-user-select:auto; /*webkit浏览器*/
+  }
+  
+  textarea{
+    -webkit-user-select:auto; /*webkit浏览器*/
   }
 
   #myChart {
